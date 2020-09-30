58a96bd9c07d9db1c5f56f8d7e7844951ee7f67f
igrlk <iluchenkov@gmail.com>
2020-09-30 22:10:09 +0300

    Delete react-app-env.d.ts

--- src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare type GraphqlMutation<TData, TVars> = (
-	options?: MutationFunctionOptions<TData, TVars> | undefined,
-) => Promise<ExecutionResult<TData>>;
